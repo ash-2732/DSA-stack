ca217be9421048d08c7862308910e4efc77f0aad
Abdullah Ashik <121299454+ash-2732@users.noreply.github.com>
2023-02-21 23:03:24 +0600

    Add files via upload

---
+++ stack_array.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+int const n = 100;
+using namespace std;
+
+
+class Stack{
+    int *element;
+    int top;
+    public:
+    Stack(){
+        element = new int[n];
+        top = -1;
+    }
+    void push( int x ){
+        if( top == n-1 ){
+            cout << "Stack Overflow" << endl;
+            return;
+        }
+        top++;
+        element[top] = x;
+    }
+    void pop(){
+        if( top == -1 ){
+            cout << "No element to pop" << endl;
+            return;
+        }
+        top--;
+    }
+    int peek(){
+        if( top == -1 ){
+            cout << "No element in stack" << endl;
+            return -1;
+        }
+        return element[top];
+    }
+    bool Empty(){
+        return top == -1;
+    }
+};
+
+int main(){
+
+    Stack st;
+    st.push( 1 );
+    st.push( 2 );
+    st.push( 3 );
+    cout << st.peek() << endl;
+    st.pop();
+    cout << st.peek() << endl;
+    st.pop();
+    st.pop();
+    st.pop();
+    cout << st.Empty() << endl;
+
+    return 0;
+}
